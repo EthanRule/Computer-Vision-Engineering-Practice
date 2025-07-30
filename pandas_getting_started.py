d74db58aa3f43fc43e2d30fc2ca1ecb54a61f5cd
EthanRule <ethan2002@live.com>
2025-07-30 11:42:47 -0700

    pandas plotting and dict work

--- pandas_getting_started.py
+++ pandas_getting_started.py
@@ -35,5 +35,58 @@
 
 # print(titanic.info())
 
+# import pandas as pd
+
+# titanic = pd.read_csv("data/titanic.csv")
+# print(titanic.head())
+
+# ages = titanic["Age"]
+# print(ages.head())
+# print(type(titanic["Age"]))
+
+# print(titanic["Age"].shape)
+
+# age_sex = titanic[["Age", "Sex"]]
+# print(age_sex.head())
+# print(type(titanic[["Age", "Sex"]]))
+
+# print(titanic[["Age", "Sex"]].shape)
+
+# above_35 = titanic[titanic["Age"] > 35]
+# print(above_35.head())
+
+# print(above_35.shape)
+
+# class_23 = titanic[titanic["Pclass"].isin([2, 3])]
+# print(class_23.head())
+
+# class_23 = titanic[(titanic["Pclass"] == 2) | (titanic["Pclass"] == 3)]
+# print(class_23.head())
+
+# age_no_na = titanic[titanic["Age"].notna()]
+# print(age_no_na.head())
+# print(age_no_na.shape)
+# cabin_no_na = titanic[titanic["Cabin"].notna()]
+# print(cabin_no_na.head())
+
+# adult_names = titanic.loc[titanic["Age"] > 35, "Name"]
+# print(adult_names.head())
+
+# print(titanic.iloc[9:25, 2:5])
+
+# titanic.iloc[0:3, 3] = "anonymous"
+# print(titanic.head())
+
 import pandas as pd
 
+import matplotlib.pyplot as plt
+
+air_quality = pd.read_csv("data/air_quality_no2.csv", index_col=0, parse_dates=True)
+
+print(air_quality.head())
+
+fig, axs = plt.subplots(figsize=(12, 4))
+air_quality.plot.area(ax=axs)
+axs.set_ylabel("NO$_2$ concentration")
+fig.savefig("no2_concentrations.png")
+plt.show()
